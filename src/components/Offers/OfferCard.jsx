f80162b9c5516365a4b35b76940879861f15da42
Gor Hakobyan <https://github.com/gorhakobyanhtml-stack/ucomReact.git>
2026-01-06 11:54:22 +0400

    feat: add OfferCard base markup

--- src/components/Offers/OfferCard.jsx
+++ src/components/Offers/OfferCard.jsx
@@ -1,15 +1,1 @@
 import styles from "./OfferCard.module.css";
-
-function OfferCard({ title, description, image }) {
-  return (
-    <div className={styles.card}>
-      <img src={image} alt={title} className={styles.image} />
-
-      <h3 className={styles.title}>{title}</h3>
-
-      <p className={styles.description}>{description}</p>
-    </div>
-  );
-}
-
-export default OfferCard;
