93da1c1c46e80528681a29d000eea83dd0ca0937
Gor Hakobyan <https://github.com/gorhakobyanhtml-stack/ucomReact.git>
2026-01-06 11:45:51 +0400

    feat: add offers mock data

---
+++ src/components/Offers/data.js
@@ -0,0 +1,8 @@
+export const offers = [
+  {
+    id: 1,
+    title: "Արագ ինտերնետ",
+    description: "Մինչև 500 Մբ/վ",
+    image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR-KF5qFXP8lgOd93PzPa8GAbfjmWvQMDrEnIX3ZIOMZcGNx1PB"
+  }
+];
